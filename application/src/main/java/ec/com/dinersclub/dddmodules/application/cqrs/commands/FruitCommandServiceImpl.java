0661dfd882cd4cdde1d1f517af4914d30271c367
devapiservice <42611322+devapiservice@users.noreply.github.com>
2021-02-02 11:12:37 -0300

    does not implement redis

--- application/src/main/java/ec/com/dinersclub/dddmodules/application/cqrs/commands/FruitCommandServiceImpl.java
+++ application/src/main/java/ec/com/dinersclub/dddmodules/application/cqrs/commands/FruitCommandServiceImpl.java
@@ -6,7 +6,7 @@ import javax.inject.Inject;
 import ec.com.dinersclub.dddmodules.application.cqrs.commands.dto.CreateFruitCommand;
 import ec.com.dinersclub.dddmodules.domain.model.Fruit;
 import ec.com.dinersclub.dddmodules.domain.repository.IPostgreSQLRepository;
-import ec.com.dinersclub.dddmodules.domain.repository.IRedisRepository;
+//import ec.com.dinersclub.dddmodules.domain.repository.IRedisRepository;
 
 @ApplicationScoped
 public class FruitCommandServiceImpl implements IFruitCommandService{
@@ -14,17 +14,17 @@ public class FruitCommandServiceImpl implements IFruitCommandService{
 	@Inject
 	private IPostgreSQLRepository fruitRepository;
 	
-	@Inject
-	private IRedisRepository redisRepository;
+	//@Inject
+	//private IRedisRepository redisRepository;
 	
 	public void createFruitCommand(CreateFruitCommand command) {
 		fruitRepository.createFruit(new Fruit(command.getId(),command.getName()));
-		redisRepository.set(String.valueOf(command.getId()), command.getName());
+		//redisRepository.set(String.valueOf(command.getId()), command.getName());
     }
 	
 	public void removeFruitCommand(int id) {
 		fruitRepository.deleteFruit(id);
-		redisRepository.del(String.valueOf(id));
+		//redisRepository.del(String.valueOf(id));
     }
 	
 }
